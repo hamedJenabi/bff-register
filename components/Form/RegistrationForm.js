5e4d49afdc3cda211927cbc21f45d087bbc5572f
Hamed <hamed.jenabi@gmail.com>
2023-11-04 20:30:54 +0100

    full

--- components/Form/RegistrationForm.js
+++ components/Form/RegistrationForm.js
@@ -50,9 +50,7 @@ export default function RegistrationForm({ form, isClicked }) {
     form.values.level === "struttin";
 
   const isDisabled = (value) =>
-    value === "beg/int" ||
-    value === "int" ||
-    (value === "texas_shuffle" && form.values.role === "lead");
+    value === "beg/int" || value === "int" || value === "texas_shuffle";
 
   return (
     <>
