29fb61df133b6c1b21e9bd8d875798eea35ecbc0
Hamed <hamed.jenabi@gmail.com>
2025-07-25 15:34:09 +0200

    paypal

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -34,7 +34,14 @@ export default async function handler(req, res) {
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card", "ideal", "sepa_debit", "sofort", "klarna"],
+      payment_method_types: [
+        "card",
+        "ideal",
+        "sepa_debit",
+        "sofort",
+        "klarna",
+        "paypal",
+      ],
       line_items: [
         {
           price_data: {
